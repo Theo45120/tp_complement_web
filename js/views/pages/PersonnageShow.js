b9c7a3fa75981527f96cdc809a2475c55a5a041a
KillianOuzet <Killianouzet@gmail.com>
2024-03-27 15:25:57 +0100

    avancement pour le PUT de la note d'un perso

--- js/views/pages/PersonnageShow.js
+++ js/views/pages/PersonnageShow.js
@@ -11,12 +11,18 @@ export default class PersonnageShow {
                 <h1> Nom du personnage : ${post.nom}</h1>
                 <h2> Classe : ${post.classe} </h2>
                 <h2> Description :  </h2>
+                <p> Une description en attente</p>
                 <h2> Statistique </h2>
                 <ul>
                     <li> Force : ${post.stats.force} </li>
                     <li> Vitesse : ${post.stats.vitesse} </li>
                     <li> Magie : ${post.stats.magie} </li>
                 </ul>
+                <div>
+                    <label for="note">Note: (0-5):</label>
+                    <input type="number" id="note" name="note" min="0" max="5" />
+                    <input type="submit" onclick="${PersonnageProvider.PutNote(request.id, document.getElementById('note').value)}">
+                </div>
             </section>
             <p><a href="/">back to home</a></p>
             <p><a href="#/articles">back to all articles</a></p>
