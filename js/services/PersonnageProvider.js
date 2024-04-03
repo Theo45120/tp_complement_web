26295c47b175b9a52ec2229476320c409c1ea3fb
KillianOuzet <Killianouzet@gmail.com>
2024-04-03 18:31:15 +0200

    ajout item

--- js/services/PersonnageProvider.js
+++ js/services/PersonnageProvider.js
@@ -50,4 +50,21 @@ export default class PersonnageProvider {
            console.log('Error getting documents', err)
        }
     }
+
+    static PutEquipement = async (id, equipement) => {
+        const options = {
+           method: 'PATCH',
+           headers: {
+               'Content-Type': 'application/json'
+           },
+           body:JSON.stringify({equipements: equipement})
+       };
+       try {
+           const response = await fetch(`${ENDPOINT}/` + id, options)
+           const json = await response.json();
+           return json
+       } catch (err) {
+           console.log('Error getting documents', err)
+       }
+    }
 }
--- js/views/pages/PersonnageShow.js
+++ js/views/pages/PersonnageShow.js
@@ -1,23 +1,11 @@
 import Utils        from '../../services/Utils.js'
 import PersonnageProvider from "../../services/PersonnageProvider.js";
-
+import EquipementProvider from "../../services/EquipementProvider.js";
 
 window.submitNote = function(id) {
     const note = document.getElementById('note').value;
     PersonnageProvider.PutNote(id, note);
 }
-
-window.add = function(id) {
-    let itemsArray = localStorage.getItem('favoris') ?
-    JSON.parse(localStorage.getItem('favoris')) : [];
-
-    if (!itemsArray.includes(id)){
-        itemsArray.push(id);
-        localStorage.setItem('favoris', JSON.stringify(itemsArray));
-    }
-    console.log(localStorage.getItem('favoris'))
-  }
-
 
 window.addOrRemoveFavorite = function(id) {
     let itemsArray = localStorage.getItem('favoris') ? JSON.parse(localStorage.getItem('favoris')) : [];
@@ -35,6 +23,35 @@ window.addOrRemoveFavorite = function(id) {
     console.log(localStorage.getItem('favoris'));
 }
 
+window.addItem = async function(id) {
+  var thelist = document.getElementById("listeItem");
+  var idx = thelist.selectedIndex;
+  var content = thelist.options[idx].innerHTML;
+  let idItem = thelist.options[idx].id;
+  console.log(content);
+  PersonnageProvider.PutEquipement(id, idItem);
+
+  let equipement = await EquipementProvider.getEquipement(idItem);
+  let divEquipement = document.getElementById("Equipement");
+  divEquipement.innerHTML = /*html*/`
+  <div class="col">
+      <div class="card shadow-sm">
+          <img src="${equipement.image}" alt="Image de l'équipement" style="width: 100%; height: 200px; object-fit: cover; border-radius:10%;">
+          <div class="card-body">
+              <p id="name" class="card-text">${equipement.nom ? equipement.nom.slice(0,100) : ''}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <a href="#/equipements/${equipement.id}" class="btn btn-sm btn-outline-secondary">Voir les statistiques</a>
+                  </div>
+                  <small class="text-body-secondary">${equipement.type}</small>
+
+              </div>
+          </div>
+      </div>
+  </div>
+  `;
+}
+
 
 export default class PersonnageShow {
 
@@ -48,6 +65,14 @@ export default class PersonnageShow {
             nameButton = "Ajouter en favoris";
         } else {
             nameButton = "Supprimer des favoris";
+        }
+
+        let equipements = await EquipementProvider.fetchEquipements(50);
+
+
+        let equipementEquipe = null;
+        if(post.equipements != [] || post.equipements != ""){
+            equipementEquipe = await EquipementProvider.getEquipement(post.equipements);
         }
         
         return /*html*/`
@@ -69,6 +94,38 @@ export default class PersonnageShow {
                     <input type="submit" onclick="submitNote('${request.id}')" value="NOTER">
                 </div>
                 <button id='favoriteButton' onclick="addOrRemoveFavorite('${request.id}')">${nameButton}<i class="fa-solid fa-plus"></i></button>
+                <div>
+                    <select id="listeItem" name="thelist">
+                    ${equipements.map(equipement => 
+                        /*html*/`
+                        <option id="${equipement.id}">${equipement.nom}</option>
+                        `
+                    ).join('\n')}
+                    </select>
+                    <button  onclick="addItem('${request.id}')">Ajouter l'équipement<i class="fa-solid fa-plus"></i></button>
+                </div>
+                <div>
+                    <h2>Equipement équipé</h2>
+                    <div id="Equipement">
+                    ${equipementEquipe != ''  ? 
+                        `<div class="col">
+                            <div class="card shadow-sm">
+                                <img src="${equipementEquipe.image}" alt="Image de l'équipement" style="width: 100%; height: 200px; object-fit: cover; border-radius:10%;">
+                                <div class="card-body">
+                                    <p id="name" class="card-text">${equipementEquipe.nom ? equipementEquipe.nom.slice(0,100) : ''}</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <a href="#/equipements/${equipementEquipe.id}" class="btn btn-sm btn-outline-secondary">Voir les statistiques</a>
+                                        </div>
+                                        <small class="text-body-secondary">${equipementEquipe.type}</small>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+                    : ''}
+                    </div>
+                </div>
             </section>
             <p><a href="/">back to home</a></p>
             <p><a href="#/articles">back to all articles</a></p>
